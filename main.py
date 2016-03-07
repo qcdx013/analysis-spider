50c440fb12bcaf59275c36b212b7483844904305
zhaopeizhi <zhaopeizhi@outlook.com>
2016-03-07 23:57:24 +0800

    add notify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import requests
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -17,4 +17,9 @@ except:
 process = CrawlerProcess(get_project_settings())
 
 process.crawl(AutohomePriceSpider)
+
+# send notify
+requests.get(
+        'http://sc.ftqq.com/SCU789T8aa9cb97fe91104109a998fed9dfab48567804bb52076.send?text=Analysis-spider-is-working!...')
+
 process.start()  # the script will block here until the crawling is finished
