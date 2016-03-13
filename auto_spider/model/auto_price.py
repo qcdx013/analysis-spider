aba659896ece5ccf56048ed64aaf6c8e4763cfee
zhaopeizhi <zhaopeizhi@outlook.com>
2016-03-13 15:32:21 +0800

    add length

--- auto_spider/model/auto_price.py
+++ auto_spider/model/auto_price.py
@@ -13,8 +13,8 @@ Base = declarative_base(metadata=metadata)
 class CarPrice(Base):
     __tablename__ = 'autohome_car_price'
 
-    spec_id = Column(String, primary_key=True)
-    city_id = Column(String, primary_key=True)
+    spec_id = Column(String(64), primary_key=True)
+    city_id = Column(String(64), primary_key=True)
     price = Column(Float)
     price_min = Column(Float)
     datetime = Column(DateTime, default=datetime.datetime.now(), primary_key=True)
@@ -23,24 +23,24 @@ class CarPrice(Base):
 class Spec(Base):
     __tablename__ = 't_specs_tmp'
 
-    brand_id = Column(String)
-    brand_name = Column(String)
-    brand_first_letter = Column(String)
-    brand_img_url = Column(String)
-    fct_name = Column(String)
-    series_id = Column(String)
-    series_name = Column(String)
-    series_img_url = Column(String)
-    spec_id = Column(String, primary_key=True)
-    spec_name = Column(String)
+    brand_id = Column(String(64))
+    brand_name = Column(String(255))
+    brand_first_letter = Column(String(8))
+    brand_img_url = Column(String(255))
+    fct_name = Column(String(255))
+    series_id = Column(String(64))
+    series_name = Column(String(255))
+    series_img_url = Column(String(255))
+    spec_id = Column(String(64), primary_key=True)
+    spec_name = Column(String(255))
 
 
 class City(Base):
     __tablename__ = 't_citys_tmp'
 
-    province_id = Column(String)
-    province_name = Column(String)
-    province_first_letter = Column(String)
-    city_id = Column(String, primary_key=True)
-    city_name = Column(String)
-    city_first_letter = Column(String)
+    province_id = Column(String(64))
+    province_name = Column(String(64))
+    province_first_letter = Column(String(8))
+    city_id = Column(String(64), primary_key=True)
+    city_name = Column(String(64))
+    city_first_letter = Column(String(8))
